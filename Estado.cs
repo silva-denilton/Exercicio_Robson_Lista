7cd1ff754f255e144463cf48ed728dc45f311648
Denilton <Denilton@DESKTOP-TU9I862>
2024-05-21 21:21:13 -0300

    Adicionar arquivos de projeto.

---
+++ Estado.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Estado
+    {
+       
+        List<Cidade> _listacidade = new List<Cidade>();
+        public void CadrastarCidade(Cidade cidade)
+        {
+            _listacidade.Add(cidade);
+        }
+        public void ImprimirCidade()
+        {
+            foreach (var cid in _listacidade)
+            {
+                Console.WriteLine(cid.Nome);
+                Console.WriteLine(cid.Populacao);
+            }
+        }
+        public Cidade LocalizarCidadeMaiorPopulacao()
+        {
+            
+            var cid = _listacidade.OrderByDescending(C => C.Populacao);
+            return (Cidade)cid;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp8;
+
+Estado esta = new Estado();
+Cidade _cidade;
+Console.WriteLine("Digite a quantidade de cidades a serem adicionadas");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    _cidade = new Cidade();
+    Console.WriteLine("Digite o nome da cidade que deseja adicionar: ");
+    _cidade.Nome = Console.ReadLine();
+    Console.WriteLine("Digite a população da cidade adicionada: ");
+    _cidade.Populacao = int.Parse(Console.ReadLine());
+    esta.CadrastarCidade(_cidade);
+}
+Console.WriteLine();
+esta.ImprimirCidade();
+esta.LocalizarCidadeMaiorPopulacao();
+
+Console.ReadLine();
+
